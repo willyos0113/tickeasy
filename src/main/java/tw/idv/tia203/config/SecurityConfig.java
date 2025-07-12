67fb92873ab4422f655b5501eea2c85c5d95988d
yiwei <willyos0113@gmail.com>
2025-07-12 22:33:11 +0800

    建立專案資料夾結構

--- src/main/java/tw/idv/tia203/config/SecurityConfig.java
+++ src/main/java/tw/idv/tia203/config/SecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
