3a35fd7f4dacdcaee69d269f605130026463adc6
yiwei <willyos0113@gmail.com>
2025-07-12 20:11:34 +0800

    初次 commit: tickeasy 專案，已實作 spring-security 及 static 靜態資源一筆 index.html

---
+++ src/main/java/tw/idv/tia203/TickeasyApplication.java
@@ -0,0 +1,14 @@
+package tw.idv.tia203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 其實就是 spring-mvc 的組態類別
+@SpringBootApplication
+public class TickeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/idv/tia203/TickeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.tia203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
